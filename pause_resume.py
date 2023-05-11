46fdd2c8b5823c3b6555824d3d7c3385a9740b90
Tao Zhang <forrest_zhang@163.com>
2023-05-11 20:52:20 +0800

    Update pause_resume.py

--- pause_resume.py
+++ pause_resume.py
@@ -6,7 +6,9 @@ from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel
 from pglive.sources.data_connector import DataConnector
 from pglive.sources.live_plot import LiveScatterPlot,LiveVBarPlot
 from pglive.sources.live_plot_widget import LivePlotWidget
+from pglive.kwargs import LeadingLine
 
+from pyqtgraph import mkPen
 
 import random
 import signal
@@ -92,7 +94,9 @@ When Live plot is paused, data are not collected.
 # Create parent widget
 widget = LivePlotWidget(title="Line Plot @ 100Hz")
 # plot = LiveVBarPlot()
-plot=LiveVBarPlot(  brush="green", pen="red",bar_width=0.01)
+# plot=LiveVBarPlot(  brush="green", pen="red",bar_width=0.01)
+plot = LiveScatterPlot(brush="yellow", pen="red", size=10)
+plot.set_leading_line(LeadingLine.HORIZONTAL, pen=mkPen("red"), text_axis=LeadingLine.AXIS_Y)
 widget.addItem(plot)
 
 # Create plot widget
@@ -101,7 +105,7 @@ parent_layout = QGridLayout()
 parent_widget.setLayout(parent_layout)
 
 # Connect plot with DataConnector
-data_connector = DataConnector(plot, max_points=600)
+data_connector = DataConnector(plot, max_points=300)
 
 # Create Pause, Resume buttons and Live status label
 pause_button = QPushButton("Pause live plot")
@@ -124,8 +128,8 @@ data_connector.sig_resumed.connect(lambda: status_label.setText("Live"))
 
 parent_widget.show()
 
-# Thread(target=randint_generator, args=(data_connector,)).start()
-Thread(target=midi_res, args=(data_connector,)).start()
+Thread(target=randint_generator, args=(data_connector,)).start()
+# Thread(target=midi_res, args=(data_connector,)).start()
 signal.signal(signal.SIGINT, lambda sig, frame: stop())
 app.exec()
 stop()
